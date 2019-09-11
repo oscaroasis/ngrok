90f9c737faa534da4321655defac73a29a033413
oscaroasis <47290487+oscaroasis@users.noreply.github.com>
2019-09-12 00:34:47 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,74 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Auto Install Tools <3
+# Coded By ØSC4R O`4S1S
+# SEMOGA BERGUNA :)
+
+# Bersihkan Layar
+clear
+
+termux-setup-storage
+
+pkg install update -y
+pkg install upgrade -y
+pkg update && pkg upgrade -y
+apt update && apt upgrade -y
+pkg install nano -y
+pkg install unzip -y
+pkg install zip -y
+pkg install unrar -y
+pkg install mpd -y
+gem install lolcat -y
+pkg install figlet -y
+pkg install ruby -y
+pkg install tree -y
+pkg install ncmpcpp -y
+pkg install libcurl -y
+pkg install wget -y
+pkg install curl -y
+pkg install openssl curl -y
+pkg install python -y
+pkg install python2 -y
+pkg install pip
+pip install --upgrade pip
+pip2 install mechanize
+pkg install aria2 -y
+pip2 install beautifulsoup
+pkg install php -y
+pkg install mc -y
+pkg install tor -y
+pkg install git -y
+apt-get -y install build-essential
+pkg install python pip install neovim
+pkg install neovim -y
+clear
+~/OSCAROASIS02
+ls
+unzip kepo.zip
+unzip kepo1.zip
+unzip kepo2.zip
+unzip kepo3.zip
+unzip kepo4.zip
+rm -rf kepo.zip
+rm -rf kepo1.zip
+rm -rf kepo2.zip
+rm -rf kepo3.zip
+rm -rf kepo4.zip
+mv 20190430_052815.jpg /sdcard
+mv RAATTACKER.jpg /sdcard
+mv LOGO.jpg /sdcard
+fi
+
+if [ $act = 22 ] || [ $act = 22  ]
+
+clear
+figlet -f small.flf  " TERIMA KASIH SUDAH MENGINSTALL NYA " | lolcat
+sleep 1
+figlet -f small.flf " BY: ØSC4R O`4S1S " | lolcat
+sleep 1
+clear
+figlet -f small.flf " SIMAK BAIK BAIK YA GUYS " | lolcat
+
+cat pemberitahuan.txt
+
+
